9143c74f1c001c17b160e879aa79cb792aa2416a
Sakari Marttinen <sakkenino@gmail.com>
2024-10-10 10:06:14 +0300

    Decrease accuracy to days

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from zoneinfo import ZoneInfo
-from datetime import datetime
+from datetime import date, datetime
 
 import dotenv
 from telebot import TeleBot
@@ -21,8 +21,9 @@ def main():
 def tj() -> int:
     # TODO: automate getting date from calendar
     tz = ZoneInfo("Europe/Helsinki")
-    today = datetime.now(tz).astimezone(tz)
-    kjyr  = datetime(today.year, 11, 16).astimezone(tz)
+    t = date.today()
+    today = datetime(t.year, t.month, t.day).astimezone(tz)
+    kjyr  = datetime(t.year, 11, 16).astimezone(tz)
     return (kjyr - today).days
 
 if __name__ == "__main__":
